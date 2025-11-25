0d19e942b67cb3378685e8412cd54d1f7820d485
injuyeon23 <injuyeon22@gmail.com>
2025-11-25 17:44:57 +0900

    [학습] JS 기본 개념

---
+++ 1.HTML/5.JS/3.EVENT/2.mouse/4.scrip.js
@@ -0,0 +1,8 @@
+        function callmyfunction() {
+            console.log("클릭되었습니다")
+        }
+        document.getElementById('myButton')
+        myButton.addEventListener('click', mouseclick);
+
+ 
+        
